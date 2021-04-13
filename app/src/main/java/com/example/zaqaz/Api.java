a55a591d3938ca5281aa58ed15fce420f8f49160
Fourcade7 <fourcadeaminov7@gmail.com>
2021-04-13 20:59:01 +0500

    retrofit2 class

---
+++ app/src/main/java/com/example/zaqaz/Api.java
@@ -0,0 +1,12 @@
+package com.example.zaqaz;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface Api {
+
+    @GET("countries")
+    Call<Location> getPosts();
+}
